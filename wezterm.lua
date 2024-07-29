2448981848368109f674d1ea652e73592436b246
jackylx2008 <jackylx2008@hotmail.com>
2024-07-29 11:24:42 +0800

    windows font size;keybindings

--- wezterm.lua
+++ wezterm.lua
@@ -8,32 +8,32 @@ local home_dir = ""
 local default_prog = {}
 
 wezterm.on("gui-startup", function()
-	local _, _, window = mux.spawn_window({})
-	window:gui_window():maximize()
+  local _, _, window = mux.spawn_window({})
+  window:gui_window():maximize()
 end)
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	home_dir = os.getenv("HOMEDRIVE") .. os.getenv("HOMEPATH") .. "\\.config\\wezterm\\pic\\"
-	font_size = 10
-	default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe" }
+  home_dir = os.getenv("HOMEDRIVE") .. os.getenv("HOMEPATH") .. "\\.config\\wezterm\\pic\\"
+  font_size = 9
+  default_prog = { "C:/Program Files/PowerShell/7/pwsh.exe" }
 
-	-- Find installed visual studio version(s) and add their compilation
-	-- environment command prompts to the menu
-	for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
-		local year = vsvers:gsub("Microsoft Visual Studio/", "")
-		table.insert(launch_menu, {
-			label = "x64 Native Tools VS " .. year,
-			args = {
-				"cmd.exe",
-				"/k",
-				"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
-			},
-		})
-	end
+  -- Find installed visual studio version(s) and add their compilation
+  -- environment command prompts to the menu
+  for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
+    local year = vsvers:gsub("Microsoft Visual Studio/", "")
+    table.insert(launch_menu, {
+      label = "x64 Native Tools VS " .. year,
+      args = {
+        "cmd.exe",
+        "/k",
+        "C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
+      },
+    })
+  end
 end
 
 if wezterm.target_triple == "x86_64-apple-darwin" then
-	home_dir = os.getenv("HOME") .. "/.config/wezterm/pic/"
+  home_dir = os.getenv("HOME") .. "/.config/wezterm/pic/"
 end
 
 -- The set of schemes that we like and want to put in our rotation
@@ -58,31 +58,31 @@ end
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- Random pic
 local file = io.open(home_dir .. "pic_list.txt", "r")
 local pic_list = {}
 if file ~= nil then
-	for line in file:lines() do
-		table.insert(pic_list, line)
-	end
-	file:close()
+  for line in file:lines() do
+    table.insert(pic_list, line)
+  end
+  file:close()
 end
 local pic = home_dir .. pic_list[math.random(#pic_list)]
 
 config.window_background_image = pic
 config.window_background_image_hsb = {
-	-- Darken the background image by reducing it to 1/3rd
-	brightness = 0.6,
+  -- Darken the background image by reducing it to 1/3rd
+  brightness = 0.6,
 
-	-- You can adjust the hue by scaling its value.
-	-- a multiplier of 1.0 leaves the value unchanged.
-	-- hue = 0.5,
+  -- You can adjust the hue by scaling its value.
+  -- a multiplier of 1.0 leaves the value unchanged.
+  -- hue = 0.5,
 
-	-- You can adjust the saturation also.
-	saturation = 1.0,
+  -- You can adjust the saturation also.
+  saturation = 1.0,
 }
 -- This is where you actually apply your config choices
 local font = "JetBrainsMonoNL Nerd Font Mono"
@@ -93,29 +93,29 @@ local font = "JetBrainsMonoNL Nerd Font Mono"
 config.native_macos_fullscreen_mode = true
 -- config.window_background_opacity = 0.80
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.font = wezterm.font(font, { weight = "Bold", italic = false })
-	config.default_prog = default_prog
+  config.font = wezterm.font(font, { weight = "Bold", italic = false })
+  config.default_prog = default_prog
 end
 if wezterm.target_triple == "x86_64-apple-darwin" then
-	config.font = wezterm.font(font, { weight = "Bold", italic = false })
+  config.font = wezterm.font(font, { weight = "Bold", italic = false })
 end
 config.color_scheme = "Google Dark (base16)"
 config.font_size = font_size
 config.native_macos_fullscreen_mode = true
 config.window_padding = {
-	left = 5,
-	right = 5,
-	top = 5,
-	bottom = 5,
+  left = 5,
+  right = 5,
+  top = 5,
+  bottom = 5,
 }
 config.use_ime = true
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_tab_bar = true
 config.show_tab_index_in_tab_bar = true
 config.inactive_pane_hsb = {
-	hue = 5.0,
-	saturation = 1.0,
-	brightness = 1.0,
+  hue = 5.0,
+  saturation = 1.0,
+  brightness = 1.0,
 }
 config.initial_cols = 90
 config.initial_rows = 40
@@ -124,45 +124,45 @@ config.exit_behavior = "Close"
 config.launch_menu = launch_menu
 config.leader = { key = ";", mods = "CTRL", timeout_milliseconds = 1500 }
 config.keys = {
-	-- New/Close pane
-	-- New tab based on current pane
-	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	-- Close current pane
-	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-	-- Close current tab
-	{ key = "x", mods = "SHIFT|CTRL", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+  -- New/Close pane
+  -- New tab based on current pane
+  { key = "c", mods = "LEADER",     action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+  -- Close current pane
+  { key = "x", mods = "LEADER",     action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+  -- Close current tab
+  { key = "x", mods = "SHIFT|CTRL", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
 
-	-- Navigate cursor between panes
-	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	-- AdjustPaneSize
-	{ key = "H", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Left", 6 } }) },
-	{ key = "J", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Down", 3 } }) },
-	{ key = "K", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Up", 3 } }) },
-	{ key = "L", mods = "LEADER", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+  -- Navigate cursor between panes
+  { key = "h", mods = "SHIFT|CTRL", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+  { key = "j", mods = "SHIFT|CTRL", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+  { key = "k", mods = "SHIFT|CTRL", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+  { key = "l", mods = "SHIFT|CTRL", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+  -- AdjustPaneSize
+  { key = "H", mods = "LEADER",     action = wezterm.action({ AdjustPaneSize = { "Left", 6 } }) },
+  { key = "J", mods = "LEADER",     action = wezterm.action({ AdjustPaneSize = { "Down", 3 } }) },
+  { key = "K", mods = "LEADER",     action = wezterm.action({ AdjustPaneSize = { "Up", 3 } }) },
+  { key = "L", mods = "LEADER",     action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
 
-	-- Toggle zoom current pane
-	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
-	-- Choose tab by leader num
-	{ key = "a", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
-	{ key = "s", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
-	-- { key = "d", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
-	-- { key = "f", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+  -- Toggle zoom current pane
+  { key = "z", mods = "LEADER",     action = "TogglePaneZoomState" },
+  -- Choose tab by leader num
+  { key = "a", mods = "LEADER",     action = wezterm.action({ ActivateTab = 0 }) },
+  { key = "s", mods = "LEADER",     action = wezterm.action({ ActivateTab = 1 }) },
+  -- { key = "d", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+  -- { key = "f", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
 
-	-- Split the pane
-	{ key = "d", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-	{ key = "f", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-	-- Toggle full screen
-	{ key = "n", mods = "SHIFT|CTRL", action = "ToggleFullScreen" },
-	-- Copy and paste to the clipboard
-	-- { key = "v", mods = "SHIFT|CTRL", action = "Paste" },
-	-- { key = "c", mods = "SHIFT|CTRL", action = "Copy" },
-	-- Navigate the tab
-	{ key = "i", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = 1 }) },
-	{ key = "o", mods = "LEADER", action = wezterm.action({ ActivateTabRelative = -1 }) },
-	{ key = "n", mods = "LEADER", action = "ShowTabNavigator" },
+  -- Split the pane
+  { key = "d", mods = "LEADER",     action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+  { key = "f", mods = "LEADER",     action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+  -- Toggle full screen
+  { key = "n", mods = "SHIFT|CTRL", action = "ToggleFullScreen" },
+  -- Copy and paste to the clipboard
+  -- { key = "v", mods = "SHIFT|CTRL", action = "Paste" },
+  -- { key = "c", mods = "SHIFT|CTRL", action = "Copy" },
+  -- Navigate the tab
+  { key = "i", mods = "LEADER",     action = wezterm.action({ ActivateTabRelative = 1 }) },
+  { key = "o", mods = "LEADER",     action = wezterm.action({ ActivateTabRelative = -1 }) },
+  { key = "n", mods = "LEADER",     action = "ShowTabNavigator" },
 }
 -- and finally, return the configuration to wezterm
 return config
